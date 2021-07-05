2783d40601d5df762e2b3b4070c897f73f352a29
haiqingd <462574808@qq.com>
2021-07-05 23:37:34 +0800

    1061

---
+++ src/main/java/PAT_Practice_1061/GenerateResult.java
@@ -0,0 +1,52 @@
+package PAT_Practice_1061;
+
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class GenerateResult {
+    public static String TEST_CASE_PATH = "/testcase.json";
+    public static void main(String[] args) {
+        Boolean isNeedGenerated = false;
+        JSONArray jsonArray = null;
+        try {
+            jsonArray = new TestMain().readTestCases(TEST_CASE_PATH);
+        } catch (IOException e) {
+            System.out.println("Json文件格式有误");
+        }
+        if (jsonArray != null) {
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                String result = Main.submit(jsonObject.getStr("input"));
+                if(!result.equals(jsonObject.getStr("answer"))){
+                    System.out.println("id : " + jsonObject.getStr("id") + "\noldAnswer:" + jsonObject.getStr("answer") + "\nnewAnswer:" + result + "\n");
+                    jsonObject.set("answer", result);
+                    isNeedGenerated = true;
+                }
+            }
+        }
+        if(! isNeedGenerated) System.out.println("所有测试用例数据均已完成且符合要求");
+        else {
+            writeToTestcase(jsonArray, TEST_CASE_PATH);
+        }
+    }
+
+    static void writeToTestcase(JSONArray jsonArray, String filepath) {
+        JSONObject jsonObject = new JSONObject(true);
+        jsonObject.set("testcase", jsonArray);
+        String jsonString = jsonObject.toStringPretty();
+        try {
+            File file = new File(System.getProperty("user.dir") + "/src/main/java/" + GenerateResult.class.getPackage().getName() + filepath);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            fileOutputStream.write(jsonString.getBytes());
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/PAT_Practice_1061/Main.java
@@ -0,0 +1,52 @@
+package PAT_Practice_1061;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static final String[] weekday = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String line = in.readLine();
+        while (!line.equals(" null")){
+            sb.append(line);
+            line = " " + in.readLine();
+        }
+        System.out.println(submit(sb.toString()));
+    }
+    static String submit(String line){
+        String[] split = line.split("\\s+");
+        boolean firstFlag = true;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < split[0].length() && i < split[1].length(); i++) {
+            if(split[0].charAt(i) == split[1].charAt(i)){
+                char same = split[0].charAt(i);
+                if(firstFlag && same >= 'A' && same <= 'G') {
+                    sb.append(weekday[same - 'A']).append(" ");
+                    firstFlag = false;
+                }else if (!firstFlag && same >= '0' && same <= '9' || same >='A' && same <='N'){
+                    if (same <= '9') sb.append(same - '0').append(":");
+                    else sb.append(same - 'A' + 10).append(":");
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < split[2].length() && i < split[3].length(); i++) {
+            if(split[2].charAt(i) == split[3].charAt(i)){
+                char same = split[2].charAt(i);
+                if(Character.isLowerCase(same) || Character.isUpperCase(same)) {
+                    sb.append(String.format("%02d",i));
+                    break;
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
