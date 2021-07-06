8792c4785a1e20185bfce250b4a996586f17fe91
haiqingd <462574808@qq.com>
2021-07-06 22:32:05 +0800

    1077 顺便重构了下答题模板

--- src/main/java/PAT_Practice_1077/Main.java
+++ src/main/java/PAT_Practice_1077/Main.java
@@ -10,10 +10,9 @@ public class Main {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         String line = in.readLine();
-        while (!line.equals(" null")) {
+        while (!line.equals("\nnull")) {
             sb.append(line);
             line = "\n" + in.readLine();
-            System.out.printf(line);
         }
         System.out.println(submit(sb.toString()));
     }
@@ -23,9 +22,9 @@ public class Main {
         int N = Integer.parseInt(split[0]);
         int shortestIndex = 0;
         for (int i = 0; i < N; i++) {
-            if (split[i+1].length() < split[shortestIndex].length() ) shortestIndex = i;
+            if (split[i+1].length() < split[shortestIndex+1].length() ) shortestIndex = i;
         }
-        StringBuilder maxSuffix = new StringBuilder(split[shortestIndex]);
+        StringBuilder maxSuffix = new StringBuilder(split[shortestIndex+1]);
         for (int i = 0; i < N; i++) {
             if (!split[i + 1].endsWith(maxSuffix.toString())) {
                 while (!split[i + 1].endsWith(maxSuffix.toString())) {
@@ -34,8 +33,9 @@ public class Main {
                 }
                 i = 0;
             }
+            if (maxSuffix.length() == 0) return "nai";
         }
 
-        return maxSuffix.length() == 0 ? "nai" : maxSuffix.toString();
+        return maxSuffix.toString();
     }
 }
