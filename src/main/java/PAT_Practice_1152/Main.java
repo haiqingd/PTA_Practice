9d1859e3aa0441f1df4210fb55f4706b54389684
haiqingd <462574808@qq.com>
2021-07-07 22:08:09 +0800

    1152 Google Recruitment

--- src/main/java/PAT_Practice_1152/Main.java
+++ src/main/java/PAT_Practice_1152/Main.java
@@ -17,6 +17,30 @@ public class Main {
         System.out.println(submit(sb.toString()));
     }
     static String submit(String line){
-        return "";
+        String[] split = line.split(" |\n");
+        int K = Integer.parseInt(split[1]);
+        for (int i = 0; i < split[2].length() - K + 1; i++) {
+            String substring = split[2].substring(i, i + K);
+            if (isPrime(Integer.parseInt(substring))) return substring;
+        }
+
+        return "404";
+    }
+
+    static boolean isPrime(int num){
+        //两个较小数另外处理
+        if(num ==2|| num==3 )
+            return true;
+        //不在6的倍数两侧的一定不是质数
+        if(num %6!= 1&&num %6!= 5)
+            return false ;
+
+        for (int i = 5; i <= Math.sqrt(num); i+=6) {
+            if(num %i== 0||num %(i+ 2)==0 )
+                return false;
+        }
+
+        return true;
+
     }
 }
