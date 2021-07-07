568fde5a4027030321693c180831216f6dc6e3a4
haiqingd <462574808@qq.com>
2021-07-07 21:21:17 +0800

    1140 Friends Number

--- src/main/java/PAT_Practice_1140/Main.java
+++ src/main/java/PAT_Practice_1140/Main.java
@@ -3,6 +3,7 @@ package PAT_Practice_1140;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 
 public class Main {
@@ -17,6 +18,23 @@ public class Main {
         System.out.println(submit(sb.toString()));
     }
     static String submit(String line){
-        return "";
+        String[] inputs = line.split("\n")[1].split(" ");
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (String input : inputs) {
+            int numberSum = 0;
+            char[] chars = input.toCharArray();
+            for (char aChar : chars) {
+                numberSum += aChar - '0';
+            }
+            map.merge(numberSum, 1, Integer::sum);
+        }
+
+        ArrayList<Integer> res = new ArrayList<>(map.keySet());
+        StringBuilder sb = new StringBuilder();
+        sb.append(res.size()).append("\n");
+        res.sort(Comparator.comparingInt(o -> o));
+        sb.append(String.join(" ", Arrays.toString(res.toArray()).split("[\\[\\]]")[1].split(", ") ));
+
+        return sb.toString();
     }
 }
--- src/main/java/PAT_Practice_1140/readme.md
+++ src/main/java/PAT_Practice_1140/readme.md
@@ -6,10 +6,23 @@
 
 ![](https://image.haiqingd.top/20210707195104.png)
 
+此题描述应该是有问题的，根据样例输出，这题的要求和朋友数无关，只需排序输出所有给出数字的各位数字之和
 ## 解题思路
 
 ## 提交过程
 
+### 1 
+![](https://image.haiqingd.top/20210707211843.png)
+
+排序错误，此时的排序按照的是String类型的默认排序
+### 2
+![](https://image.haiqingd.top/20210707211922.png)
+
+
 ## 总结
 
 ## 参考文献
+
+[java - 将int数组转换为String数组](https://www.coder.work/article/75513)
+
+[【Java】ArrayList<String>转化为String数组问题](https://blog.csdn.net/chuyouyinghe/article/details/72833502)
