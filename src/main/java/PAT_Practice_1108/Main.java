aa05a83636d6fc5431680c0c144fb51bc787cf61
haiqingd <462574808@qq.com>
2021-07-07 20:40:25 +0800

    1108 计算平均数

--- src/main/java/PAT_Practice_1108/Main.java
+++ src/main/java/PAT_Practice_1108/Main.java
@@ -10,13 +10,39 @@ public class Main {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         String line = in.readLine();
-        while (!line.equals("\nnull")){
+        while (!line.equals("\nnull")) {
             sb.append(line);
             line = "\n" + in.readLine();
         }
         System.out.println(submit(sb.toString()));
     }
-    static String submit(String line){
-        return "";
+
+    static String submit(String line) {
+        StringBuilder sb = new StringBuilder();
+        if(Integer.parseInt(line.split("\n")[0]) == 0) return "The average of 0 numbers is Undefined";
+        String[] inputs = (line.split("\n")[1]).split(" ");
+        int legalNums = 0;
+        double averData = 0;
+
+        for (String input : inputs) {
+            double data = 0;
+            try {
+                data = Double.parseDouble(input);
+                String[] split = input.split("\\.");
+                if ( split.length > 1 && split[1].length() > 2) throw new Exception();
+                if (data > 1000 || data < -1000) throw new Exception();
+            } catch (Exception e) {
+                sb.append("ERROR: ").append(input).append(" is not a legal number\n");
+                continue;
+            }
+            averData = (legalNums * averData + data) / ++legalNums;
+        }
+
+        if (legalNums == 1)sb.append("The average of ").append(legalNums).append(" number is ");
+        else sb.append("The average of ").append(legalNums).append(" numbers is ");
+        if (legalNums > 0) sb.append(String.format("%.2f",averData));
+        else sb.append("Undefined");
+
+        return sb.toString();
     }
 }
